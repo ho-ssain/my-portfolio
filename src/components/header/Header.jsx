a03e92b259348fe9f890c1b2ca2145a90107aca8
Hossain K. M. <awarehossain@gmail.com>
2024-03-08 17:24:13 +0600

    initialized repo

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,67 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav-logo">
+          Hossain
+        </a>
+
+        <div className="nav-menu">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <a href="#home" className="nav-link active-link">
+                <i className="uil uil-estate nav-icon"></i> Home
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a href="#about" className="nav-link">
+                <i className="uil uil-user nav-icon"></i> About
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a href="#skills" className="nav-link">
+                <i className="uil uil-file-alt nav-icon"></i> Skills
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a href="#education" className="nav-link">
+                <i className="uil uil-university nav-icon"></i> Education
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a href="#services" className="nav-link">
+                <i className="uil uil-briefcase-alt nav-icon"></i> Services
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a href="#portfolio" className="nav-link">
+                <i className="uil uil-scenery nav-icon"></i> Portfolio
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a href="#contact" className="nav-link">
+                <i className="uil uil-message nav-icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+
+          <i className="uil uil-times nav-close"></i>
+        </div>
+
+        <div className="nav-toggle">
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
